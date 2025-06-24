7b598aef75dba04e6ffa9ba7415cdf0fa911f7ba
Anastasia Kim <kim.anastasia2000@gmail.com>
2025-06-24 16:09:16 +1000

    fix(cors): add permissions policy to resolve browser warnings

--- supabase/functions/_shared/cors.ts
+++ supabase/functions/_shared/cors.ts
@@ -3,4 +3,5 @@
 export const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
+  'Permissions-Policy': 'geolocation=(), microphone=(), camera=()', // Explicitly set a policy to override the default and remove browser warnings.
 };
